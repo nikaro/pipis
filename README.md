008bc18bca834deeb66cc1ac7de0ea4840e90876
Nicolas KAROLAK <nicolas@karolak.fr>
2018-05-17 18:52:38 +0200

    release v1.0.0

--- README.md
+++ README.md
@@ -7,11 +7,17 @@
 
 Actually it is a rewrite of [pipsi](https://github.com/mitsuhiko/pipsi) but with [venv](https://docs.python.org/dev/library/venv.html) instead of [virtualenv](https://virtualenv.pypa.io/en/stable/).
 
-## Why ?
+## What does it do?
+
+Pipis is a wrapper around venv and pip which installs scripts provided by python packages into separate venvs to shield them from your system and each other.
+
+It creates a venv in `~/.local/venvs/`, update pip, installs the package, and links the package's scripts to `~/.local/bin/`.
+
+## Why not pipsi?
 
 Because i do not care about Python 2, and `virtualenv` copies python's binaries while `venv` just symlink them (which i think is cleaner, but it still copies `pip` which is not clean…).
 
-## Installation
+## How to install
 
 ```
 python3 -m venv ~/.local/venvs/pipis
@@ -20,9 +26,105 @@ pip install pipis
 deactivate
 ```
 
-## Usage
+## How to update
 
 ```
-pipis --help
-pipis install django
+pipis update pipis
 ```
+
+## How to uninstall
+
+```
+pipis uninstall pipis
+```
+
+## How to use
+
+### Show help
+
+```
+$ pipis --help
+Usage: pipis [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  install
+  list
+  uninstall
+  update
+```
+
+You can also invoke `--help` on each commands.
+
+### Install package(s)
+
+```
+$ pipis install ansible
+Do you want to continue? [y/N]: y
+Installing  [####################################]  100%
+```
+
+You can install multiple packages:
+
+```
+$ pipis install ansible ansible-lint jmespath
+Do you want to continue? [y/N]: y
+Installing  [####################################]  100%
+```
+
+### Unattended install package(s)
+
+```
+$ pipis install --yes awscli
+Installing  [####################################]  100%
+```
+
+# Update package(s)
+
+```
+$ pipis update ansible
+Do you want to continue? [y/N]: y
+Updating  [####################################]  100%
+```
+
+You can also update all installed packages at once:
+
+```
+$ pipis update
+Do you want to continue? [y/N]: y
+Updating  [####################################]  100%
+```
+
+### List installed packages
+
+```
+$ pipis list
+Installed:
+  - ansible
+  - ansible-lint
+  - awscli
+  - bashate
+  - docker-compose
+  - flake8
+  - jmespath
+  - pipis
+  - poetry
+  - pylint
+  - python-language-server
+  - twine
+```
+
+### Uninstall package(s)
+
+```
+$ pipis uninstall ansible
+Do you want to continue? [y/N]: y
+Removing  [####################################]  100%
+```
+
+## Credits
+
+- [Armin Ronacher](https://github.com/mitsuhiko): the author of pipsi, for the inspiration
+- [Nicolas Karolak](): myself, the author of pipis
--- pipis/__init__.py
+++ pipis/__init__.py
@@ -1,11 +1,54 @@
 import click
+from operator import methodcaller
 import os
-import shutil
-import subprocess
+from shutil import rmtree
+from subprocess import check_call, check_output
 import sys
-import venv
+from venv import create
 
-VENV_ROOT_DIR = os.path.expanduser('~/.local/venvs')
+VENV_ROOT_DIR = os.path.expanduser('~/.local/venvs/')
+BIN_DIR = os.path.expanduser('~/.local/bin/')
+
+
+def _show_package(package):
+    if package is not None:
+        return package
+
+
+def _get_console_scripts(venv_dir, venv_py, package):
+    venv_bin_dir = os.path.join(venv_dir, 'bin')
+    # get the module path from its venv and append it to the current path
+    venv_path = check_output(
+        [venv_py, '-c', 'import sys; print(sys.path[-1])']).decode("utf-8").strip()
+    sys.path.append(venv_path)
+    # get informations about package
+    import pkg_resources
+    dist = pkg_resources.get_distribution(package)
+    # init list
+    entry_points = []
+    # get entry_points from RECORD file
+    if dist.has_metadata('RECORD'):
+        files = dist.get_metadata_lines('RECORD')
+        for line in files:
+            line = os.path.join(dist.location, line.split(',')[0])
+            line = os.path.normpath(line)
+            entry_points.append(line)
+    # get entry_points from installed-files.txt file
+    elif dist.has_metadata('installed-files.txt'):
+        files = dist.get_metadata_lines('installed-files.txt')
+        for line in files:
+            line = os.path.join(dist.egg_info, line.split(',')[0])
+            line = os.path.normpath(line)
+            entry_points.append(line)
+    # get entry_points from entry_points.txt file
+    elif dist.has_metadata('entry_points.txt'):
+        entry_points = dist.get_entry_map('console_scripts').keys()
+
+    # filter only binaries
+    entry_points = list(filter(methodcaller(
+        'startswith', venv_bin_dir), entry_points))
+
+    return entry_points
 
 
 @click.group()
@@ -16,30 +59,39 @@ def cli():
 @cli.command('list')
 def list_installed():
     click.echo('Installed:')
-    for item in sorted(os.listdir(VENV_ROOT_DIR)):
-        click.echo('  - {}'.format(item))
+    for package in sorted(os.listdir(VENV_ROOT_DIR)):
+        click.echo('  - {}'.format(package))
 
 
 @cli.command()
 @click.confirmation_option()
 @click.argument('name', nargs=-1, required=True, type=click.STRING)
 def install(name):
-    click.echo('Install: {}'.format(', '.join(sorted(name))))
-    with click.progressbar(name) as packages:
-        for item in packages:
-            venv_path = os.path.join(VENV_ROOT_DIR, item)
-            venv_python = os.path.join(venv_path, 'bin', 'python')
-            cmd = [venv_python, '-m', 'pip', 'install', '--quiet']
+    with click.progressbar(name, label='Installing', item_show_func=_show_package) as packages:
+        for package in packages:
+            venv_dir = os.path.join(VENV_ROOT_DIR, package)
+            venv_py = os.path.join(venv_dir, 'bin', 'python')
+            cmd = [venv_py, '-m', 'pip', 'install', '--quiet']
             # create venv if not exists
-            if not os.path.exists(venv_path):
-                venv.create(venv_path, symlinks=True, with_pip=True)
+            if not os.path.isdir(venv_dir):
+                create(venv_dir, symlinks=True, with_pip=True)
                 # upgrade pip in venv
                 cmd.extend(['--upgrade', 'pip'])
-                subprocess.check_call(cmd)
-            # install package in venv
-            cmd.append(item)
-            subprocess.check_call(cmd)
-            # TODO: install binary symlink
+                check_call(cmd)
+                # install package in venv
+                cmd.append(package)
+                check_call(cmd)
+                # create scripts symlink
+                scripts = _get_console_scripts(venv_dir, venv_py, package)
+                for script in scripts:
+                    script_name = script.split('/')[-1]
+                    link = os.path.join(BIN_DIR, script_name)
+                    if not os.path.islink(link):
+                        os.symlink(script, link)
+            else:
+                click.secho(' Info: {} is already installed, skipping it'.format(
+                    package), fg='green')
+
 
 
 @cli.command()
@@ -48,32 +100,51 @@ def install(name):
 def update(name):
     if not name:
         name = os.listdir(VENV_ROOT_DIR)
-    click.echo('Update: {}'.format(', '.join(sorted(name))))
-    with click.progressbar(name) as packages:
-        for item in packages:
-            venv_path = os.path.join(VENV_ROOT_DIR, item)
-            venv_python = os.path.join(venv_path, 'bin', 'python')
-            cmd = [venv_python, '-m', 'pip', 'install', '--quiet']
+    with click.progressbar(name, label='Updating', item_show_func=_show_package) as packages:
+        for package in packages:
+            venv_dir = os.path.join(VENV_ROOT_DIR, package)
+            if not os.path.isdir(venv_dir):
+                click.secho(' Warning: {} is not installed, skipping it'.format(
+                    package), fg='yellow')
+                continue
+            venv_py = os.path.join(venv_dir, 'bin', 'python')
+            cmd = [venv_py, '-m', 'pip', 'install', '--quiet']
             # upgrade pip in venv
             cmd.extend(['--upgrade', 'pip'])
-            subprocess.check_call(cmd)
+            check_call(cmd)
             # install package in venv
-            cmd.extend(['--upgrade', item])
-            subprocess.check_call(cmd)
-            # TODO: update binary symlink
+            cmd.extend(['--upgrade', package])
+            check_call(cmd)
+            # update scripts symlink
+            scripts = _get_console_scripts(venv_dir, venv_py, package)
+            for script in scripts:
+                script_name = script.split('/')[-1]
+                link = os.path.join(BIN_DIR, script_name)
+                if not os.path.islink(link):
+                    os.symlink(script, link)
 
 
 @cli.command()
 @click.confirmation_option()
 @click.argument('name', nargs=-1, required=True, type=click.STRING)
 def uninstall(name):
-    click.echo('Uninstall: {}'.format(', '.join(sorted(name))))
-    with click.progressbar(name) as packages:
-        for item in packages:
-            # remove package venv
-            venv_path = os.path.join(VENV_ROOT_DIR, item)
-            shutil.rmtree(venv_path)
-            # TODO: remove binary symlink
+    with click.progressbar(name, label='Removing', item_show_func=_show_package) as packages:
+        for package in packages:
+            venv_dir = os.path.join(VENV_ROOT_DIR, package)
+            venv_py = os.path.join(venv_dir, 'bin', 'python')
+            if os.path.isdir(venv_dir):
+                # remove scripts symlink
+                scripts = _get_console_scripts(venv_dir, venv_py, package)
+                for script in scripts:
+                    script_name = script.split('/')[-1]
+                    link = os.path.join(BIN_DIR, script_name)
+                    if os.path.islink(link):
+                        os.remove(link)
+                # remove package venv
+                rmtree(venv_dir)
+            else:
+                click.secho(' Warning: {} is not installed, skipping it'.format(
+                    package), fg='yellow')
 
 if __name__ == '__main__':
     cli()
