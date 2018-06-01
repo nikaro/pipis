a45af442dee8b5b21003b94551cff42bd49b8861
Nicolas KAROLAK <nicolas@karolak.fr>
2018-06-01 18:40:07 +0200

    black formatting

--- pipis/__init__.py
+++ pipis/__init__.py
@@ -14,15 +14,13 @@ import click
 import pkg_resources
 from tabulate import tabulate
 
-DEFAULT_PIPIS_VENVS = '~/.local/venvs/'
-DEFAULT_PIPIS_BIN = '~/.local/bin/'
+DEFAULT_PIPIS_VENVS = "~/.local/venvs/"
+DEFAULT_PIPIS_BIN = "~/.local/bin/"
 
 
 def _set_pipis_vars():
-    pipis_venvs = os.path.expanduser(
-        os.environ.get('PIPIS_VENVS', DEFAULT_PIPIS_VENVS))
-    pipis_bin = os.path.expanduser(
-        os.environ.get('PIPIS_BIN', DEFAULT_PIPIS_BIN))
+    pipis_venvs = os.path.expanduser(os.environ.get("PIPIS_VENVS", DEFAULT_PIPIS_VENVS))
+    pipis_bin = os.path.expanduser(os.environ.get("PIPIS_BIN", DEFAULT_PIPIS_BIN))
 
     return pipis_venvs, pipis_bin
 
@@ -30,7 +28,7 @@ def _set_pipis_vars():
 def _get_venv_data(package):
     pipis_venvs, _ = _set_pipis_vars()
     venv_dir = os.path.join(pipis_venvs, package)
-    venv_py = os.path.join(venv_dir, 'bin', 'python')
+    venv_py = os.path.join(venv_dir, "bin", "python")
 
     return venv_dir, venv_py
 
@@ -38,8 +36,8 @@ def _get_venv_data(package):
 def _get_dist(package):
     _, venv_py = _get_venv_data(package)
     # get the module path from its venv and append it to the current path
-    cmd_last_path = 'import sys; print(sys.path[-1])'
-    venv_path = check_output([venv_py, '-c', cmd_last_path])
+    cmd_last_path = "import sys; print(sys.path[-1])"
+    venv_path = check_output([venv_py, "-c", cmd_last_path])
     venv_path = venv_path.decode("utf-8").strip()
     sys.path.append(venv_path)
     # reload pkg_resources module to take into account new path
@@ -65,26 +63,27 @@ def _get_console_scripts(package):
     # init list
     entry_points = []
     # get entry_points from RECORD file
-    if dist.has_metadata('RECORD'):
-        files = dist.get_metadata_lines('RECORD')
+    if dist.has_metadata("RECORD"):
+        files = dist.get_metadata_lines("RECORD")
         for line in files:
-            line = os.path.join(dist.location, line.split(',')[0])
+            line = os.path.join(dist.location, line.split(",")[0])
             line = os.path.normpath(line)
             entry_points.append(line)
     # get entry_points from installed-files.txt file
-    elif dist.has_metadata('installed-files.txt'):
-        files = dist.get_metadata_lines('installed-files.txt')
+    elif dist.has_metadata("installed-files.txt"):
+        files = dist.get_metadata_lines("installed-files.txt")
         for line in files:
-            line = os.path.join(dist.egg_info, line.split(',')[0])
+            line = os.path.join(dist.egg_info, line.split(",")[0])
             line = os.path.normpath(line)
             entry_points.append(line)
     # get entry_points from entry_points.txt file
-    elif dist.has_metadata('entry_points.txt'):
-        entry_points = dist.get_entry_map('console_scripts').keys()
+    elif dist.has_metadata("entry_points.txt"):
+        entry_points = dist.get_entry_map("console_scripts").keys()
 
     # filter only binaries
-    entry_points = list(filter(methodcaller(
-        'startswith', os.path.join(venv_dir, 'bin')), entry_points))
+    entry_points = list(
+        filter(methodcaller("startswith", os.path.join(venv_dir, "bin")), entry_points)
+    )
 
     return entry_points
 
@@ -98,7 +97,7 @@ def _show_package(package):
     return package
 
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -122,16 +121,16 @@ def version():
     click.echo(package_version)
 
 
-@cli.command('list', context_settings=CONTEXT_SETTINGS)
+@cli.command("list", context_settings=CONTEXT_SETTINGS)
 def list_installed():
     """List installed packages."""
     pipis_venvs, _ = _set_pipis_vars()
-    table = {'Package': [], 'Version': []}
+    table = {"Package": [], "Version": []}
     for package in sorted(os.listdir(pipis_venvs)):
         package_version = _get_version(package)
-        table['Package'].append(package)
-        table['Version'].append(package_version)
-    click.echo(tabulate(table, headers='keys'))
+        table["Package"].append(package)
+        table["Version"].append(package_version)
+    click.echo(tabulate(table, headers="keys"))
 
 
 @cli.command(context_settings=CONTEXT_SETTINGS)
@@ -140,18 +139,27 @@ def freeze():
     pipis_venvs, _ = _set_pipis_vars()
     for package in sorted(os.listdir(pipis_venvs)):
         package_version = _get_version(package)
-        click.echo('{}=={}'.format(package, package_version))
+        click.echo("{}=={}".format(package, package_version))
 
 
-@cli.command(context_settings=CONTEXT_SETTINGS, short_help='Install packages.')
-@click.option('-y', '--yes', is_flag=True, callback=_abort_if_false,
-              prompt='Do you want to continue?', expose_value=False,
-              help='Confirm the action without prompting.')
-@click.option('-r', '--requirement',
-              help='Install from the given requirements file.')
-@click.option('-s', '--system-site-packages', is_flag=True,
-              help='Give the virtual environment access to the system site-packages dir.')
-@click.argument('name', nargs=-1, type=click.STRING)
+@cli.command(context_settings=CONTEXT_SETTINGS, short_help="Install packages.")
+@click.option(
+    "-y",
+    "--yes",
+    is_flag=True,
+    callback=_abort_if_false,
+    prompt="Do you want to continue?",
+    expose_value=False,
+    help="Confirm the action without prompting.",
+)
+@click.option("-r", "--requirement", help="Install from the given requirements file.")
+@click.option(
+    "-s",
+    "--system-site-packages",
+    is_flag=True,
+    help="Give the virtual environment access to the system site-packages dir.",
+)
+@click.argument("name", nargs=-1, type=click.STRING)
 def install(requirement, name):
     """
     Install packages, where NAME is the package name.
@@ -162,28 +170,34 @@ def install(requirement, name):
     _, pipis_bin = _set_pipis_vars()
     # check presence of args
     if not (requirement or name):
-        raise click.UsageError('missing arguments/options')
+        raise click.UsageError("missing arguments/options")
     # check mutually esclusive args
     if requirement and name:
-        raise click.UsageError('too many arguments/options')
+        raise click.UsageError("too many arguments/options")
     # populate packages list with req file
     if requirement:
         try:
-            with open(requirement, 'r') as req:
+            with open(requirement, "r") as req:
                 name = map(str.strip, req.readlines())
         except IOError:
             raise click.FileError(requirement)
     # process packages
-    with click.progressbar(name, label='Installing',
-                           item_show_func=_show_package) as packages:
+    with click.progressbar(
+        name, label="Installing", item_show_func=_show_package
+    ) as packages:
         for package in packages:
             venv_dir, venv_py = _get_venv_data(package)
-            cmd = [venv_py, '-m', 'pip', 'install', '--quiet']
+            cmd = [venv_py, "-m", "pip", "install", "--quiet"]
             # create venv if not exists
             if not os.path.isdir(venv_dir):
-                create(venv_dir, symlinks=True, with_pip=True, system_site_packages=system_site_packages)
+                create(
+                    venv_dir,
+                    symlinks=True,
+                    with_pip=True,
+                    system_site_packages=system_site_packages,
+                )
                 # upgrade pip in venv
-                cmd.extend(['--upgrade', 'pip'])
+                cmd.extend(["--upgrade", "pip"])
                 check_call(cmd)
                 # install package in venv
                 cmd.append(package)
@@ -191,34 +205,39 @@ def install(requirement, name):
                     check_call(cmd)
                 except CalledProcessError:
                     rmtree(venv_dir)
-                    message = 'Cannot install {}'.format(package)
+                    message = "Cannot install {}".format(package)
                     raise click.BadArgumentUsage(message)
                 # create scripts symlink
                 scripts = _get_console_scripts(package)
                 if len(scripts) < 1:
                     rmtree(venv_dir)
-                    message = 'library installation is not supported by pipis'
+                    message = "library installation is not supported by pipis"
                     raise click.ClickException(message)
                 for script in scripts:
-                    script_name = script.split('/')[-1]
+                    script_name = script.split("/")[-1]
                     link = os.path.join(pipis_bin, script_name)
                     try:
                         os.symlink(script, link)
                     except FileExistsError:
                         rmtree(venv_dir)
-                        message = '{} already exists'.format(link)
+                        message = "{} already exists".format(link)
                         raise click.ClickException(message)
             else:
-                click.secho(' is already installed, skip', fg='green')
+                click.secho(" is already installed, skip", fg="green")
 
 
-@cli.command(context_settings=CONTEXT_SETTINGS, short_help='Update packages.')
-@click.option('-y', '--yes', is_flag=True, callback=_abort_if_false,
-              prompt='Do you want to continue?', expose_value=False,
-              help='Confirm the action without prompting.')
-@click.option('-r', '--requirement',
-              help='Install from the given requirements file.')
-@click.argument('name', nargs=-1, type=click.STRING)
+@cli.command(context_settings=CONTEXT_SETTINGS, short_help="Update packages.")
+@click.option(
+    "-y",
+    "--yes",
+    is_flag=True,
+    callback=_abort_if_false,
+    prompt="Do you want to continue?",
+    expose_value=False,
+    help="Confirm the action without prompting.",
+)
+@click.option("-r", "--requirement", help="Install from the given requirements file.")
+@click.argument("name", nargs=-1, type=click.STRING)
 def update(requirement, name):
     """
     Update packages, where NAME is the package name.
@@ -232,48 +251,53 @@ def update(requirement, name):
     pipis_venvs, pipis_bin = _set_pipis_vars()
     # check mutually esclusive args
     if requirement and name:
-        raise click.UsageError('too many arguments/options')
+        raise click.UsageError("too many arguments/options")
     # populate packages list with req file
     if requirement:
         try:
-            with open(requirement, 'r') as req:
+            with open(requirement, "r") as req:
                 name = map(str.strip, req.readlines())
         except IOError:
             raise click.FileError(requirement)
     # populate packages list with all currently installed
     if not name:
         name = os.listdir(pipis_venvs)
-    with click.progressbar(name, label='Updating',
-                           item_show_func=_show_package) as packages:
+    with click.progressbar(
+        name, label="Updating", item_show_func=_show_package
+    ) as packages:
         for package in packages:
             venv_dir, venv_py = _get_venv_data(package)
             if not os.path.isdir(venv_dir):
-                message = '{} is not installed'.format(package)
+                message = "{} is not installed".format(package)
                 raise click.ClickException(message)
-            cmd = [venv_py, '-m', 'pip', 'install', '--quiet']
+            cmd = [venv_py, "-m", "pip", "install", "--quiet"]
             # upgrade pip in venv
-            cmd.extend(['--upgrade', 'pip'])
+            cmd.extend(["--upgrade", "pip"])
             check_call(cmd)
             # install package in venv
-            cmd.extend(['--upgrade', package])
+            cmd.extend(["--upgrade", package])
             check_call(cmd)
             # update scripts symlink
             scripts = _get_console_scripts(package)
             for script in scripts:
-                script_name = script.split('/')[-1]
+                script_name = script.split("/")[-1]
                 link = os.path.join(pipis_bin, script_name)
                 if not os.path.islink(link):
                     os.symlink(script, link)
 
 
-@cli.command(context_settings=CONTEXT_SETTINGS,
-             short_help='Uninstall packages.')
-@click.option('-y', '--yes', is_flag=True, callback=_abort_if_false,
-              prompt='Do you want to continue?', expose_value=False,
-              help='Confirm the action without prompting.')
-@click.option('-r', '--requirement',
-              help='Install from the given requirements file.')
-@click.argument('name', nargs=-1, type=click.STRING)
+@cli.command(context_settings=CONTEXT_SETTINGS, short_help="Uninstall packages.")
+@click.option(
+    "-y",
+    "--yes",
+    is_flag=True,
+    callback=_abort_if_false,
+    prompt="Do you want to continue?",
+    expose_value=False,
+    help="Confirm the action without prompting.",
+)
+@click.option("-r", "--requirement", help="Install from the given requirements file.")
+@click.argument("name", nargs=-1, type=click.STRING)
 def uninstall(requirement, name):
     """
     Uninstall packages, where NAME is the package name.
@@ -284,34 +308,35 @@ def uninstall(requirement, name):
     pipis_venvs, pipis_bin = _set_pipis_vars()
     # check presence of args
     if not (requirement or name):
-        raise click.UsageError('missing arguments/options')
+        raise click.UsageError("missing arguments/options")
     # check mutually esclusive args
     if requirement and name:
-        raise click.UsageError('too many arguments/options')
+        raise click.UsageError("too many arguments/options")
     # populate packages list with req file
     if requirement:
         try:
-            with open(requirement, 'r') as req:
+            with open(requirement, "r") as req:
                 name = map(str.strip, req.readlines())
         except IOError:
             raise click.FileError(requirement)
-    with click.progressbar(name, label='Removing',
-                           item_show_func=_show_package) as packages:
+    with click.progressbar(
+        name, label="Removing", item_show_func=_show_package
+    ) as packages:
         for package in packages:
             venv_dir = os.path.join(pipis_venvs, package)
             if os.path.isdir(venv_dir):
                 # remove scripts symlink
                 scripts = _get_console_scripts(package)
                 for script in scripts:
-                    script_name = script.split('/')[-1]
+                    script_name = script.split("/")[-1]
                     link = os.path.join(pipis_bin, script_name)
                     if os.path.islink(link):
                         os.remove(link)
                 # remove package venv
                 rmtree(venv_dir)
             else:
-                click.secho(' is not installed, skip', fg='yellow')
+                click.secho(" is not installed, skip", fg="yellow")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
